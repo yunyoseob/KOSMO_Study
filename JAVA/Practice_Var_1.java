33f03c717dcebbe3cf4b08abb029fd8b3df0d340
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-01-28 16:40:51 +0900

    Add files via upload

---
+++ JAVA/Practice_Var_1.java
@@ -0,0 +1,32 @@
+package a.b.c.d.ch1;
+// package: 접근제한자
+// a,b,c,d,ch1 이렇게 쓰면 안 된다.
+// 도트 연산자로 구분할 것
+// a.b.c.d.ch1
+// ;은 종결자
+// 반드시 쓸 것
+
+
+public class Practice_Var_1{
+	public static void main(String args[]){
+		System.out.println("Hello");
+		// System 밖으로 나올 때 반드시, 종결자 ;(세미클론) 찍을 것
+		String s="안녕하세요";
+		// 변수 선언시에도 반드시, 종결자 ;(세미클론) 찍을 것
+		System.out.println(s);
+		int i=10;
+		System.out.println(i);	
+	}// 메서드 닫기
+
+}// 클래스 닫기
+
+
+
+
+
+
+
+
+
+
+
