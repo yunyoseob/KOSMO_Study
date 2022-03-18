781a28f664b0be11315f6658a8a9e96ba13be5c1
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-03-18 12:48:09 +0900

    Add files via upload

--- ISUD_PRACTICE/TEST_T6/src/a/b/c/t6/servie/T6ServiceImpl.java
+++ ISUD_PRACTICE/TEST_T6/src/a/b/c/t6/servie/T6ServiceImpl.java
@@ -1,4 +1,4 @@
-package a.b.c.t6.service;
+package a.b.c.t6.servie;
 
 import java.util.ArrayList;
 
@@ -7,62 +7,64 @@ import a.b.c.t6.dao.T6DAOImpl;
 import a.b.c.t6.vo.T6VO;
 
 public class T6ServiceImpl implements T6Service {
+
 	@Override
-	public ArrayList<T6VO> t6SelectAll(){
-		System.out.println("\n T6ServiceImpl :: t6SelectAll() :: 시작");
+	public ArrayList<T6VO> t6SelectAll() {
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6SelectAll >>> :");
 		
-		T6DAO td=new T6DAOImpl();
-		
-		return td.t6SelectAll();
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6SelectAll();
 	}
-	
+
 	@Override
 	public ArrayList<T6VO> t6SelectName(T6VO tvo) {
-		System.out.println("\n T6ServiceImpl :: t6SelectAll() :: 시작");
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6SelectAll >>> :");
 		
-		T6DAO td=new T6DAOImpl();
-		ArrayList<T6VO> aList=td.t6SelectName(tvo);
-		
-		return aList;
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6SelectName(tvo);
 	}
-	
+
 	@Override
 	public ArrayList<T6VO> t6SelectDate(T6VO tvo) {
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6SelectAll >>> :");
 		
-		T6DAO td=new T6DAOImpl();
-		ArrayList<T6VO> aList=td.t6SelectDate(tvo);
-		
-		return aList;
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6SelectDate(tvo);
 	}
-	
-	
+
+	// 입력하기 
 	@Override
 	public boolean t6Insert(T6VO tvo) {
-		T6DAO td=new T6DAOImpl();
-		boolean bool=false;
-		bool=td.t6Insert(tvo);
-
-		return bool;
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6Insert >>> : tvo " + tvo);
+		T6VO.printlnT6VO(tvo);
+		
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6Insert(tvo);
 	}
-	
-	
+
+	// 수정하기 
 	@Override
 	public boolean t6Update(T6VO tvo) {
-		T6DAO td=new T6DAOImpl();
-		boolean bool=false;
-		bool=td.t6Update(tvo);
-
-		return bool;
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6Update >>> : tvo " + tvo);
+		T6VO.printlnT6VO(tvo);
+		
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6Update(tvo);
 	}
-	
-	
+
+	// 삭제하기 
 	@Override
 	public boolean t6Delete(T6VO tvo) {
-		T6DAO td=new T6DAOImpl();
-		boolean bool=false;
-		bool=td.t6Delete(tvo);
+		// TODO Auto-generated method stub
+		System.out.println("T6ServiceImpl.t6Delete >>> : tvo " + tvo);
+		T6VO.printlnT6VO(tvo);
 		
-		return bool;
+		T6DAO tdao = new T6DAOImpl();
+		return tdao.t6Delete(tvo);
 	}
-	
-} // T6ServiceImpl
+}
