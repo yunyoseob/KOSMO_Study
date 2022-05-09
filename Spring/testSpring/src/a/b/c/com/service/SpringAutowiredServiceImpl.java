d81f1635ee2e83b59ee9fb19f56b24dc9fc69bcc
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-05-09 18:13:11 +0900

    Add files via upload

---
+++ Spring/testSpring/src/a/b/c/com/service/SpringAutowiredServiceImpl.java
@@ -0,0 +1,37 @@
+package a.b.c.com.service;
+
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import a.b.c.com.dao.SpringAutowiredFiledDAO;
+import a.b.c.com.vo.FormDataVO;
+
+@Service
+@Transactional
+public class SpringAutowiredServiceImpl implements SpringAutowiredService {
+	private Logger logger = LogManager.getLogger(SpringAutowiredFieldServiceImpl.class);
+	
+	
+	private SpringAutowiredFiledDAO springAutowiredFiledDAO;
+	
+	// 오토와이어드 어노테이션 생성자 주입 
+	@Autowired(required=false)
+	public SpringAutowiredServiceImpl(SpringAutowiredFiledDAO springAutowiredFiledDAO) {
+		this.springAutowiredFiledDAO = springAutowiredFiledDAO;
+	}
+
+	@Override
+	public List<FormDataVO> autowiredTest(FormDataVO fvo) {
+		// TODO Auto-generated method stub
+		logger.info("SpringAutowiredFieldServiceImpl.autowiredTest_Get() 함수 진입");
+		
+		List<FormDataVO> list = springAutowiredFiledDAO.autowiredTest(fvo);
+		return list;
+	}
+
+}
