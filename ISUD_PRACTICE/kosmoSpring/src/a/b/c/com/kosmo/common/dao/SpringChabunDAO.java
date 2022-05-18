d8a811f22f2dbbc39d70c9320ad3ba40af69fbb3
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-05-18 18:24:05 +0900

    Delete ISUD_PRACTICE/kosmoSpring directory

--- ISUD_PRACTICE/kosmoSpring/src/a/b/c/com/kosmo/common/dao/SpringChabunDAO.java
+++
@@ -1,8 +0,0 @@
-package a.b.c.com.kosmo.common.dao;
-
-import a.b.c.com.kosmo.product.vo.KosmoProductVO;
-
-public interface SpringChabunDAO {
-
-	public KosmoProductVO getProductChabun();
-}
--- ISUD_PRACTICE/kosmoSpring/src/a/b/c/com/kosmo/common/service/SpringChabunService.java
+++
@@ -1,8 +0,0 @@
-package a.b.c.com.kosmo.common.service;
-
-import a.b.c.com.kosmo.product.vo.KosmoProductVO;
-
-public interface SpringChabunService {
-
-	public KosmoProductVO getProductChabun();
-}
