52a711fe379e402b426c8d210efab7e528329ef8
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-05-18 18:26:03 +0900

    Add files via upload

---
+++ ISUD_PRACTICE/kosmoSpring/WebContent/js/common.js
@@ -0,0 +1,22 @@
+
+
+function comma(s){
+	let result = s.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	console.log(result);
+	return result;
+}
+
+
+function comma_replace(s){
+	let result = s.toString().replace(/,/g, "");
+	console.log(result);
+	return result;
+}
+
+
+function numpad(s){	
+	if (s.length == 1){
+		s = '0' + s;
+	}	
+	return s;
+}
